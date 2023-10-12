560673639b8be4c1805cef3961f4ee52a1be7a9a
Uzielmf <Uziel.munozf@gmail.com>
2023-10-12 09:49:56 -0600

    se cambia la fucnion get data a el arcvhivo app

--- countris-api/src/App.jsx
+++ countris-api/src/App.jsx
@@ -3,13 +3,26 @@ import './App.css'
 import useData from './services/useData'
 
 function App() {
+  const [data, setData] = useState(null)
   
+    const getData = async () => {
+      const res = await fetch('https://restcountries.com/v3.1/all');
+      const datos = await res.json();
+      setData(datos)
+    }
+
+    useEffect(() =>{
+    getData()
+  }, [])
+
+  return data;
+}
 
   return (
     <>
      
     </>
   )
-}
+
 
 export default App
